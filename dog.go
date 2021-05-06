082eb41e5b2cbddbd45b807cf423c8d7922e1583
林玮 (Jade Lin) <linw1995@icloud.com>
2021-05-06 23:57:34 +0800

    Add Sniffing func for ProcessAliveDog

--- dog.go
+++ dog.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"context"
 	"os"
 	"syscall"
+	"time"
 )
 
 // Dog for checking if process is alive or not via its Pid.
@@ -17,7 +19,20 @@ func (dog ProcessAliveDog) Sniff() bool {
 	return err == nil
 }
 
+// Keep checking if process is alive or not via its Pid.
+func (dog ProcessAliveDog) Sniffing(ctx context.Context, internal time.Duration, resultChannel chan bool) {
+	for {
+		select {
+		case <-ctx.Done():
+			break
+		default:
+			resultChannel <- dog.Sniff()
+		}
+		time.Sleep(internal)
+	}
+}
+
 // Create a ProcessAliveDog via the Pid of a process it is watching.
-func NewProcessAliveDog(pid int) *ProcessAliveDog{
+func NewProcessAliveDog(pid int) *ProcessAliveDog {
 	return &ProcessAliveDog{Pid: pid}
 }
--- dog_test.go
+++ dog_test.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"context"
 	"os/exec"
 	"testing"
+	"time"
 )
 
 func TestProcessAliveDog(t *testing.T) {
@@ -39,4 +41,41 @@ func TestProcessAliveDog(t *testing.T) {
 
 		},
 	)
+
+	t.Run("Keep checking", func(t *testing.T) {
+		cmd := exec.Command("sleep", "60")
+		err := cmd.Start()
+		if err != nil {
+			t.Errorf(`Run command "sleep 60" error %v`, err)
+		}
+
+		p := cmd.Process
+		dog := NewProcessAliveDog(p.Pid)
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+		isProcessAliveChannel := make(chan bool)
+
+		go dog.Sniffing(ctx, 1 * time.Second, isProcessAliveChannel)
+
+		want := true
+		got := <-isProcessAliveChannel
+		if got != want {
+			t.Errorf("got %v, want %v", got, want)
+		}
+		err = p.Kill()
+		if err != nil {
+			t.Errorf(`Kill command "sleep 60" error %v`, err)
+		}
+		_, err = p.Wait()
+		if err != nil {
+			t.Errorf(`Wait command "sleep 60" error %v`, err)
+		}
+
+		want = false
+		got = <-isProcessAliveChannel
+		if got != want {
+			t.Errorf("got %v, want %v", got, want)
+		}
+
+	})
 }
