1ce46e2aefc702a0e265c11c3895892cc0fd2c98
林玮 (Jade Lin) <linw1995@icloud.com>
2021-05-06 22:23:12 +0800

    Handle all the returned errors

--- dog_test.go
+++ dog_test.go
@@ -6,23 +6,37 @@ import (
 )
 
 func TestProcessAliveDog(t *testing.T) {
-	cmd := exec.Command("sleep", "60")
-	cmd.Start()
-	p := cmd.Process
+	t.Run("Check if process is alive or not via its Pid",
+		func(t *testing.T) {
+			cmd := exec.Command("sleep", "60")
+			err := cmd.Start()
+			if err != nil {
+				t.Errorf(`Run command "sleep 60" error %v`, err)
+			}
 
-	dog := NewProcessAliveDog(p.Pid)
-	want := true
-	got := dog.Sniff()
-	if got != want {
-		t.Errorf("got %v, want %v", got, want)
-	}
+			p := cmd.Process
+			dog := NewProcessAliveDog(p.Pid)
+			want := true
+			got := dog.Sniff()
+			if got != want {
+				t.Errorf("got %v, want %v", got, want)
+			}
 
-	p.Kill()
-	p.Wait()
+			err = p.Kill()
+			if err != nil {
+				t.Errorf(`Kill command "sleep 60" error %v`, err)
+			}
+			_, err = p.Wait()
+			if err != nil {
+				t.Errorf(`Wait command "sleep 60" error %v`, err)
+			}
 
-	want = false
-	got = dog.Sniff()
-	if got != want {
-		t.Errorf("got %v, want %v", got, want)
-	}
+			want = false
+			got = dog.Sniff()
+			if got != want {
+				t.Errorf("got %v, want %v", got, want)
+			}
+
+		},
+	)
 }
