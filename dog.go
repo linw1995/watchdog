18a76b05ac6b695165918ca68f6036df4588c671
林玮 (Jade Lin) <linw1995@icloud.com>
2021-05-07 15:30:10 +0800

    Refactor and add Dog interface

--- dog.go
+++ dog.go
@@ -10,6 +10,23 @@ type Sleeper interface {
 	Sleep()
 }
 
+type Dog interface {
+	Sniff() bool
+}
+
+// Keep sniffing
+func Sniffing(ctx context.Context, dog Dog, sleeper Sleeper, resultChannel chan bool) {
+	for {
+		select {
+		case <-ctx.Done():
+			break
+		default:
+			resultChannel <- dog.Sniff()
+		}
+		sleeper.Sleep()
+	}
+}
+
 // Dog for checking if process is alive or not via its Pid.
 type ProcessAliveDog struct {
 	Pid int
@@ -20,19 +37,6 @@ func (dog ProcessAliveDog) Sniff() bool {
 	p, _ := os.FindProcess(dog.Pid)
 	err := p.Signal(syscall.Signal(0))
 	return err == nil
-}
-
-// Keep checking if process is alive or not via its Pid.
-func (dog ProcessAliveDog) Sniffing(ctx context.Context, sleeper Sleeper, resultChannel chan bool) {
-	for {
-		select {
-		case <-ctx.Done():
-			break
-		default:
-			resultChannel <- dog.Sniff()
-		}
-		sleeper.Sleep()
-	}
 }
 
 // Create a ProcessAliveDog via the Pid of a process it is watching.
--- dog_test.go
+++ dog_test.go
@@ -52,6 +52,50 @@ func newSleeperWithControl() *sleeperWithControl {
 	return &sleeperWithControl{controlChannel: make(chan interface{})}
 }
 
+type dogWithControl struct {
+	flag bool
+}
+
+func (dog *dogWithControl) Sniff() bool {
+	return dog.flag
+}
+
+func (dog *dogWithControl) Sniffed() {
+	dog.flag = true
+}
+
+func (dog *dogWithControl) Unsniffed() {
+	dog.flag = false
+}
+
+func newDogWithControl(initFlag bool) *dogWithControl {
+	return &dogWithControl{flag: initFlag}
+}
+
+func TestSniffing(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	dog := newDogWithControl(true)
+	sleeper := newSleeperWithControl()
+	resultChannel := make(chan bool)
+
+	go Sniffing(ctx, dog, sleeper, resultChannel)
+
+	want := true
+	got := <-resultChannel
+	if got != want {
+		t.Errorf("got %v, want %v", got, want)
+	}
+
+	dog.Unsniffed()
+	sleeper.Awake()
+	want = false
+	got = <-resultChannel
+	if got != want {
+		t.Errorf("got %v, want %v", got, want)
+	}
+}
+
 func TestProcessAliveDog(t *testing.T) {
 	t.Run("Check if process is alive or not via its Pid", func(t *testing.T) {
 		dp := newDummyProcess("sleep", "60")
@@ -83,7 +127,7 @@ func TestProcessAliveDog(t *testing.T) {
 		isProcessAliveChannel := make(chan bool)
 
 		sleeper := newSleeperWithControl()
-		go dog.Sniffing(ctx, sleeper, isProcessAliveChannel)
+		go Sniffing(ctx, dog, sleeper, isProcessAliveChannel)
 
 		want := true
 		got := <-isProcessAliveChannel
