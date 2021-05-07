e4d0ab131e9c78398e7b543a899cfc3e2f7abada
林玮 (Jade Lin) <linw1995@icloud.com>
2021-05-07 10:58:01 +0800

    Refactor and add dummyProcess

--- dog_test.go
+++ dog_test.go
@@ -7,69 +7,75 @@ import (
 	"time"
 )
 
+type dummyProcess struct {
+	cmd *exec.Cmd
+}
+
+func (dp *dummyProcess) Start() {
+	err := dp.cmd.Start()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (dp *dummyProcess) Stop() {
+	err := dp.cmd.Process.Kill()
+	if err != nil {
+		panic(err)
+	}
+	_, err = dp.cmd.Process.Wait()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (dp *dummyProcess) Pid() int {
+	return dp.cmd.Process.Pid
+}
+
+func newDummyProcess(name string, args ...string) *dummyProcess {
+	return &dummyProcess{cmd: exec.Command(name, args...)}
+}
+
 func TestProcessAliveDog(t *testing.T) {
-	t.Run("Check if process is alive or not via its Pid",
-		func(t *testing.T) {
-			cmd := exec.Command("sleep", "60")
-			err := cmd.Start()
-			if err != nil {
-				t.Errorf(`Run command "sleep 60" error %v`, err)
-			}
+	t.Run("Check if process is alive or not via its Pid", func(t *testing.T) {
+		dp := newDummyProcess("sleep", "60")
+		dp.Start()
 
-			p := cmd.Process
-			dog := NewProcessAliveDog(p.Pid)
-			want := true
-			got := dog.Sniff()
-			if got != want {
-				t.Errorf("got %v, want %v", got, want)
-			}
-
-			err = p.Kill()
-			if err != nil {
-				t.Errorf(`Kill command "sleep 60" error %v`, err)
-			}
-			_, err = p.Wait()
-			if err != nil {
-				t.Errorf(`Wait command "sleep 60" error %v`, err)
-			}
-
-			want = false
-			got = dog.Sniff()
-			if got != want {
-				t.Errorf("got %v, want %v", got, want)
-			}
-
-		},
-	)
-
-	t.Run("Keep checking", func(t *testing.T) {
-		cmd := exec.Command("sleep", "60")
-		err := cmd.Start()
-		if err != nil {
-			t.Errorf(`Run command "sleep 60" error %v`, err)
+		dog := NewProcessAliveDog(dp.Pid())
+		want := true
+		got := dog.Sniff()
+		if got != want {
+			t.Errorf("got %v, want %v", got, want)
 		}
 
-		p := cmd.Process
-		dog := NewProcessAliveDog(p.Pid)
+		dp.Stop()
+
+		want = false
+		got = dog.Sniff()
+		if got != want {
+			t.Errorf("got %v, want %v", got, want)
+		}
+
+	})
+
+	t.Run("Keep checking", func(t *testing.T) {
+		dp := newDummyProcess("sleep", "60")
+		dp.Start()
+		dog := NewProcessAliveDog(dp.Pid())
 		ctx, cancel := context.WithCancel(context.Background())
 		defer cancel()
 		isProcessAliveChannel := make(chan bool)
 
-		go dog.Sniffing(ctx, 1 * time.Second, isProcessAliveChannel)
+		go dog.Sniffing(ctx, 1*time.Second, isProcessAliveChannel)
 
 		want := true
 		got := <-isProcessAliveChannel
 		if got != want {
 			t.Errorf("got %v, want %v", got, want)
 		}
-		err = p.Kill()
-		if err != nil {
-			t.Errorf(`Kill command "sleep 60" error %v`, err)
-		}
-		_, err = p.Wait()
-		if err != nil {
-			t.Errorf(`Wait command "sleep 60" error %v`, err)
-		}
+
+		dp.Stop()
 
 		want = false
 		got = <-isProcessAliveChannel
